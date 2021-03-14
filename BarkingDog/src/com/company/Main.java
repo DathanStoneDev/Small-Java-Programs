c4a331995e4bc415312687ca3c0ba8c8b67304da
Dathan Stone <73630546+DathanStoneDev@users.noreply.github.com>
2021-03-14 13:49:34 -0700

    Add files via upload

---
+++ BarkingDog/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    boolean result = BarkingDog.shouldWakeUp(true, 9);
+	    System.out.println(result);
+    }
+}
