f42261bf2d76b728bdd2884e97ddca265fd33e8f
Dathan Stone <73630546+DathanStoneDev@users.noreply.github.com>
2021-03-18 05:22:39 -0700

    Add files via upload

---
+++ AreaCalculator/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(-1));
+        System.out.println(AreaCalculator.area(-1, 23.4));
+
+    }
+}
---
+++ PlayingCat/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+       boolean playingCat = PlayingCat.isCatPlaying(false, 35);
+       System.out.println(playingCat);
+    }
+}
